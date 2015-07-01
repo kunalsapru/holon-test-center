b498476352dc8f51648b18c3c0782ebe2472aed7
Ritika <ritika.2734@gmail.com>
2015-07-02 01:19:43 +0200

    UI Changes

--- htc/WebContent/js/googleMaps.js
+++ htc/WebContent/js/googleMaps.js
@@ -176,20 +176,10 @@ $(document)
 						//START KUNAL CODE>>>DONT EDIT/REMOVE IT
 				    	  var latSouthWest = newShape.getBounds().getSouthWest().lat();
 				    	  var lngSouthWest = newShape.getBounds().getSouthWest().lng();
-
-				    	//END KUNAL CODE>>>DONT EDIT/REMOVE IT
-				    	  markerslat.push(latNorthEast); //add data to array
-				    	  markerslng.push(lngNorthEast); //add data to array
-						//START KUNAL CODE>>>DONT EDIT/REMOVE IT
-
 				    	  $("#holonObjectLatitudeNE").text(latNorthEast);
 				    	  $("#holonObjectLongitudeNE").text(lngNorthEast);
 				    	  $("#holonObjectLatitudeSW").text(latSouthWest);
 				    	  $("#holonObjectLongitudeSW").text(lngSouthWest);
-
-				    	  //END KUNAL CODE>>>DONT EDIT/REMOVE IT
-
-				    	  infowindowArray.push("");
 				    	  if(clickedValuePanel=="addHolonObject"){
 				    		  getHolonObjectTypeFromDatabase();
 				    		  getHolonCoordinatorFromDatabase();
@@ -202,108 +192,19 @@ $(document)
 				      
 				    	  //When Rectangle is clicked
 				    	  google.maps.event.addListener(newShape, 'click', function() {
-				    		  $("#displayHolonDetails").empty();
-				    	 
-				    	//to check this event for each newshape clicked
-				    		 var latValueofRectangle = newShape.getBounds().getNorthEast().lat();
-				    		for(var i=0;i<markerslat.length;i++)
-				    		if(latValueofRectangle==markerslat[i])	//Traverse array for the clicked rectangle
-				    		{
-				    			//No element Added
-				    			var clicked = i;
-				    			if(infowindowArray[i]=="" ||infowindowArray[i]== undefined || infowindowArray[i]== null )
-				    				{
-				    					//alert("No element for this Holon Object");
-				    				}
-				    			else{
-				    				
-				    				//Data exist in the array
-				    				//Code to show data in infoWindow 
-				    				if(infowindow)
-									{infowindow.close();}
-				    				//Content for the info window separated by : then ,
-				    				var holonDetail=infowindowArray[i].split("@");
-				    				var data="";
-				    				var dataForInfoWindow="";
-				    				for(var k=0;k<holonDetail.length-1;k++)
-				    					{
-				    						var individualElementDetail=holonDetail[k].split("#");
-				    						for(var l=0;l<individualElementDetail.length;l++)
-				    							{
-				    								if(k==0){
-				    									dataForInfoWindow = dataForInfoWindow+"</br>"+individualElementDetail[l];
-				    								}
-				    							    
-				    								data= data+"</br>"+individualElementDetail[l];
-				    							}
-				    					/*var content=	"<div data-role='collapsible' id='displayHolonDetails" + k + "'><h3>Holon Element " + (k+1) + "</h3><p>"+data+"</p></div>";
-				    						$("#displayHolonDetails").append( content ).collapsibleset('refresh');*/
-				    						data="";
-				    					}
-				    				if(k>1)
-				    					{
-				    					dataForInfoWindow=dataForInfoWindow+"</br></br>"+"<a href='#' id='moreInfo' value="+i+">More Information</a>";
-				    					}
-				    				
-				    				infowindow = new google.maps.InfoWindow({
-				    				      content: dataForInfoWindow,
-				    				      position: newShape.getBounds().getNorthEast()
-				    				  });
-				    				
-				    				infowindow.open(map,newShape);
+				    		  console.log("New Shape::"+newShape);
+				    		  for(var key in newShape) {
+				    			    var value = newShape[key];
+				    			    console.log(value);
 				    			}
-				    		}
-				    	 
+				    		  //ajaxRequest("showHolonObjects", {}, showHolonObjectsCallBack, {});
 				    	  });
 				    	 
 				  	});
 				     //END of overlay Complete 
 					  // Select the data in the info window
 					  $("#saveElementInfo").click(function(){
-						  //$('#dropDownId :selected').text();
-						  var textvalue= "<b>Element Type-</b>"+$("#elementType").find('option:selected').text();
-						  $(".elementType").change(function() {
-							        alert($('.elementType option:selected').html());
-							    });
-						 
-						 var elementState= "<b>Element State-</b>"+"overSupply";
-						 $('#elementState').on('change', function() {
-							  textvalue="<b>Element State-</b>"+$(this).val(); // or $(this).val()
-							});
-						 var holonManager="<b>Holon Manager-</b>"+"Manager1";
-						 $('#holonManager').on('change', function() {
-							  textvalue="<b>Holon Manager-</b>"+$(this).val(); // or $(this).val()
-							});
-						 var holonCoordinator="<b>Holon Coordinator-</b>"+$("#holonCoordinator").val();
-						 var minCapacity="<b>Min Capacity-</b>"+ $("#minCapacity").val();
-						 var maxCapacity= "<b>Max. Capacity-</b>"+$("#maxCapacity").val();
-						 var usage= "<b>Usage-</b>"+$("#usage").val();
-						 var currentCapacity="<b>Current Capacity-</b>"+$("#currentCapacity").val();
-						 var currentEnergyStatus="<b>Current Energy Status-</b>"+$("#currentEnergyStatus").val();
-						 var result=textvalue+"#"+elementState+"#"+holonManager+"#"+holonCoordinator+"#"+minCapacity+"#"+maxCapacity+"#"+usage+"#"+currentCapacity+"#"+currentEnergyStatus;
-						 
-						for(var j=0;j<markerslat.length;j++)
-							{
-								if(clickedToAddElements==markerslat[j])
-									{
-											//get value of the previous from the array
-											var valueFromArray=result+"@"+infowindowArray[j];
-											infowindowArray[j]=valueFromArray;
-											$("#elementName").html("");
-											
-									}
-							}
-						console.log(infowindowArray);
-						console.log(infowindowArray.length);
-						 //Clear the value and hide the div
-						// $("#elementName").val("");
-						 $("#minCapacity").val("");
-						 $("#maxCapacity").val("");
-						 $("#usage").val("");
-						 $("#holonCoordinator").val("");
-						 $("#currentCapacity").val("");
-						 $("#currentEnergyStatus").val("");
-						 $("#displayHolonDetails").empty();						  
+						  alert("Clicked");
 					  });
 					  //End of Save Button
 					  
@@ -622,7 +523,7 @@ $("#holonCoordinatorInformation").popup("open");
 
 function getHolonCoordinatorFromDatabase()
 {
-	ajaxRequest("getListHolonCoordinator", {}, getHolonCoordinatorFromDatabaseCallBack, {});
+	ajaxRequest("getListHolonCoordinator", {}, getHolonCoordinatorFromDatabaseCallBack,{});
 }
 
 function getHolonCoordinatorFromDatabaseCallBack(data,options)
