a83f23b12de870a7044a0f7ef9a59a1c0905a1e0
Abhinav <Abhinav@AbhinavThinkPad>
2015-07-02 01:48:19 +0200

    Changes according to the DB Change

--- htc/src/com/htc/action/HolonCoordinatorAction.java
+++ htc/src/com/htc/action/HolonCoordinatorAction.java
@@ -17,18 +17,21 @@ public class HolonCoordinatorAction extends CommonUtilities{
 	
 	public void createHolonCoordinator(){
 		try{
-		/*Double latNE = getRequest().getParameter("latNE")!=null?Double.parseDouble(getRequest().getParameter("latNE")):0D;
+		Double latNE = getRequest().getParameter("latNE")!=null?Double.parseDouble(getRequest().getParameter("latNE")):0D;
 		Double lngNE = getRequest().getParameter("lngNE")!=null?Double.parseDouble(getRequest().getParameter("lngNE")):0D;
-		Double latSW = getRequest().getParameter("latSW")!=null?Double.parseDouble(getRequest().getParameter("latSW")):0D;
-		Double lngSW = getRequest().getParameter("lngSW")!=null?Double.parseDouble(getRequest().getParameter("lngSW")):0D;*/
+		//Double latSW = getRequest().getParameter("latSW")!=null?Double.parseDouble(getRequest().getParameter("latSW")):0D;
+		//Double lngSW = getRequest().getParameter("lngSW")!=null?Double.parseDouble(getRequest().getParameter("lngSW")):0D;
 		Integer coordinatorOfHolon = getRequest().getParameter("coordinatorHolon")!=null?Integer.parseInt(getRequest().getParameter("coordinatorHolon")):0;
 		String nameCoordinator= getRequest().getParameter("nameCoordinator")!=null?getRequest().getParameter("nameCoordinator"):"";
-		LatLng latLng = new LatLng();
-		Integer locationId = getLatLngService().persist(latLng);
-		LatLng latLng2 = getLatLngService().findById(locationId);
+		LatLng NeLatLng = new LatLng(latNE, lngNE);
+		//LatLng SeLatLng = new LatLng(latNE, lngNE);
+		Integer NeLocationId = getLatLngService().persist(NeLatLng);
+		//Integer SeLocationId = getLatLngService().persist(SeLatLng);
+		LatLng NeLatLng2 = getLatLngService().findById(NeLocationId);
+		//LatLng SeLatLng2 = getLatLngService().findById(SeLocationId);
 		Holon holon = new Holon(coordinatorOfHolon);
 		HolonCoordinator holonCoordinator = new HolonCoordinator();
-		holonCoordinator.setLatLng(latLng2);
+		holonCoordinator.setLatLng(NeLatLng2);
 		holonCoordinator.setName(nameCoordinator);
 		holonCoordinator.setHolon(holon);
 		Integer newHolonCoordinatortID = getHolonCoordinatorService().persist(holonCoordinator);
