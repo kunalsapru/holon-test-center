a127e6381643660e5c4468c24bb8e04fc62396b6
Abhinav <Abhinav@AbhinavThinkPad>
2015-07-17 01:06:27 +0200

    switch on and off on GUI

--- htc/WebContent/js/switch.js
+++ htc/WebContent/js/switch.js
@@ -14,8 +14,8 @@ function drawSwitchOnPowerLine(clickedValue){
 function createPowerSwitch(latLng,powerLineId){
 	var circleSwitch = new google.maps.Circle({
 		 strokeColor: '#FF0000',
-	     strokeOpacity: 0.8,
-	     strokeWeight: 2,
+	     strokeOpacity: 1,
+	     strokeWeight: 8,
 	     fillColor: '#FF0000',
 	     fillOpacity: 0.35,
 	     map: map,
@@ -27,13 +27,16 @@ function createPowerSwitch(latLng,powerLineId){
 			switchPositionLng : latLng.lng(),
 			powerLineId : powerLineId
 	};
-	ajaxRequest("createPowerSwitch", dataAttributes, createPowerSwitchCallBack, {});
+	ajaxRequest("createPowerSwitch", dataAttributes, createPowerSwitchCallBack, {powerLineId : powerLineId,circleSwitch:circleSwitch});
 }
 
 function createPowerSwitchCallBack(data,options)
 {
-	console.log("Switch Added in Database Succesfully");
-	
+	var powerSwitchId=data;	
+	var powerLineId = options["powerLineId"];
+	var circleSwitch = options["circleSwitch"]; 
+	var contentString="Power Line Id : "+powerLineId+"</br>"+"Switch Id: "+powerSwitchId+"</br></br></br>"+" <input type='button' value='Toggle Switch' id='togglePowerSwitch' />";
+	addSwitchInfo(contentString, circleSwitch, powerSwitchId);
 }
 
 function showSavedPowerSwitches(){
@@ -61,7 +64,7 @@ function getListPowerSwitchCallBack(data,options){
 		     center: new google.maps.LatLng(switchLat, switchLong),
 		     radius: 2
 		    });	
-		var contentString="Power Line Id : "+powerLineId+"</br>"+"Switch Id: "+switchId+"</br></br></br>"+" <input type='button' value='Toggle Switch' id='togglePowerSwitch' onclick='SwitchOnOff()' />";
+		var contentString="Power Line Id : "+powerLineId+"</br>"+"Switch Id: "+switchId+"</br></br></br>"+" <input type='button' value='Toggle Switch' id='togglePowerSwitch' />";
 		addSwitchInfo(contentString, circleSwitch, switchId);
 		
 		}
@@ -75,14 +78,30 @@ function addSwitchInfo(contentString, marker, id)
 	  });
    google.maps.event.addListener(marker, 'click', function(event) {   	
 		infowindowHolonObject.setOptions({position:event.latLng});
-		infowindowHolonObject.open(map,map);	
+		infowindowHolonObject.open(map,marker);	
+		$('#togglePowerSwitch').click(function() {
+			SwitchOnOff(marker);			
+		})
+		
    });
 }
 
-function SwitchOnOff()
-{
-	 
-	alert ("circleSwitch.get('fillColor')");
+function SwitchOnOff(marker)
+{	 
+
+	var switchStatusColor = marker.get('fillColor');
+	if(switchStatusColor=="#FF0000")
+		{
+		//alert("Color Red");
+		marker.setOptions({strokeColor:'#0B6121', fillColor: '#0B6121'});
+		
+		}else
+		{
+			//alert("Color Green");
+			marker.setOptions({strokeColor:'#FF0000',fillColor: '#FF0000'});
+		}
+	
+
 	
 	
 }
