e77163fd46d926015d2a8080ca343f5aca1d2d80
Ritika <ritika.2734@gmail.com>
2015-08-12 20:33:12 +0200

    For Consumption Graph

--- htc/WebContent/js/consumptionGraph.js
+++ htc/WebContent/js/consumptionGraph.js
@@ -5,102 +5,120 @@ function showConsumptionGraph(holonObjectId)
 	var dataAttributes= {
 			holonObjectId : holonObjectId
 	}
-	ajaxRequest("showHolonElements", dataAttributes, getDataForChartCallBack, dataAttributes);
+	ajaxRequest("showHolonObjectConsumption", dataAttributes, getDataForConsumptionGraphCallBack, dataAttributes);
 //	openDiv("consumptionGraphBody");
 	
  //alert(holonObjectId);	
 }
 
-function getDataForChartCallBack(data,options)
+function getDataForConsumptionGraphCallBack(data,options)
 {
+	var graphForConsumption;
+	var holonObjectId=data.split("*")[1];
+	var currentConsumption=parseInt(data.split("*")[0]);
+	console.log(holonObjectId+"****"+currentConsumption+"****"+data);
 	openDiv("consumptionGraphBody");
-	var graphForConsumption= new Highcharts.Chart({
-        chart: {
-        	renderTo: "consumptionGraphBody",
-            type: 'spline',
-            animation: Highcharts.svg, // don't animate in old IE
-            marginRight: 10,
-            events: {
-                load: function () {
+	if(currentConsumption==0 || holonObjectId==undefined ){
+		alert("No Holon Elements to show");
+	closeDiv("consumptionGraphBody");
+	}
+	else{
+		//openDiv("consumptionGraphBody");
+		
+		Highcharts.setOptions({
+	        global: {
+	            useUTC: false
+	        }
+	    });
+		var graphForConsumption= new Highcharts.Chart({
+	        chart: {
+	        	renderTo: "consumptionGraphBody",
+	            type: 'spline',
+	            animation: Highcharts.svg, // don't animate in old IE
+	            marginRight: 10,
+	            events: {
+	                load: function () {
+	                	// set up the updating of the chart each second
+	                    var series = this.series[0];
 
-                    // set up the updating of the chart each second
-                    var series = this.series[0];
-                    setInterval(function () {
-                        var x = (new Date()).getTime(), // current time
-                            y = Math.random();
+	                	if(series) { setInterval(function () {
+	                        var x = (new Date()).getTime(), // current time
+                            y = currentConsumption+ Math.floor((Math.random() * 10) + 1);
                         series.addPoint([x, y], true, true);
-                    }, 1000);
-                }
-            }
-        },
-        title: {
-            text: 'Consumption Graph'
-        },
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150
-        },
-        yAxis: {
-            title: {
-                text: 'Value'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            formatter: function () {
-                return '<b>' + this.series.name + '</b><br/>' +
-                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                    Highcharts.numberFormat(this.y, 2);
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        exporting: {
-            enabled: false
-        },
-        series: [{
-            name: 'Element Consumption',
-            data: (function () {
-                // generate an array of random data
-                var data1 = [],
-                    time = (new Date()).getTime(),
-                    i;
+                    }, 1000); }	                    
+	                }
+	            }
+	        },
+	        title: {
+	            text: 'Consumption Graph of Holon : '+holonObjectId
+	        },
+	        xAxis: {
+	            type: 'datetime',
+	            tickPixelInterval: 150
+	        },
+	        yAxis: {
+	            title: {
+	                text: 'Consumption'
+	            },
+	            plotLines: [{
+	                value: 0,
+	                width: 1,
+	                color: '#808080'
+	            }]
+	        },
+	        tooltip: {
+	            formatter: function () {
+	                return '<b>' + this.series.name + '</b><br/>' +
+	                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+	                    Highcharts.numberFormat(this.y, 2);
+	            }
+	        },
+	        legend: {
+	            enabled: false
+	        },
+	        exporting: {
+	            enabled: false
+	        },
+	        series: [{
+	            name: 'Element Consumption',
+	            data: (function () {
+	                // generate an array of random data
+	                var data1 = [],
+	                    time = (new Date()).getTime(),
+	                    i;
 
-                for (i = -19; i <= 0; i += 1) {
-                    data1.push({
-                        x: time + i * 1000,
-                        y: Math.random()
-                    });
-                }
-                return data1; 
-            }())
-        }],
-        exporting: {
-            buttons: {
-            	contextButton: {
-                    enabled: false
-                },
-                closeButton: {
-                    text: 'Close',
-                    onclick: function () {
-                    	closeDiv('consumptionGraphBody');
-                    }
-                },
-                exportButton: {
-                    text: 'Download',
-                    // Use only the download related menu items from the default context button
-                    menuItems: Highcharts.getOptions().exporting.buttons.contextButton.menuItems.splice(2)
-                }
-                
-            }
-        }
-    });
-    
+	                for (i = -19; i <= 0; i += 1) {
+	                    data1.push({
+	                        x: time + i * 1000,
+	                        y: currentConsumption + Math.floor((Math.random() * 10) + 1)
+	                    });
+	                }
+	                return data1; 
+	            }())
+	        }],
+	        exporting: {
+	            buttons: {
+	            	contextButton: {
+	                    enabled: false
+	                },
+	                closeButton: {
+	                    text: 'Close',
+	                    onclick: function () {
+	                    	closeDiv('consumptionGraphBody');
+	                    }
+	                },
+	                exportButton: {
+	                    text: 'Download',
+	                    // Use only the download related menu items from the default context button
+	                    menuItems: Highcharts.getOptions().exporting.buttons.contextButton.menuItems.splice(2)
+	                }
+	                
+	            }
+	        }
+	    });
+
+	}
+	    
 	//Render To consumptionGraphBody
 	console.log(data);
 }
--- htc/src/com/htc/action/ConsumptionGraphAction.java
+++ htc/src/com/htc/action/ConsumptionGraphAction.java
@@ -1,8 +1,42 @@
 package com.htc.action;
 
-public class ConsumptionGraphAction {
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
+import com.htc.hibernate.pojo.HolonElement;
+import com.htc.hibernate.pojo.HolonObject;
+import com.htc.utilities.CommonUtilities;
+
+public class ConsumptionGraphAction extends CommonUtilities {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public void showConsumptionGraph(){
 		
+		
+		Logger log = Logger.getLogger(ConsumptionGraphAction.class);
+		
+		try{
+			
+			Integer holonObjectId = getRequest().getParameter("holonObjectId")!=null?Integer.parseInt(getRequest().getParameter("holonObjectId")):0;
+			HolonObject holonObject = getHolonObjectService().findById(holonObjectId);		 
+			ArrayList<HolonElement> holonElementList = getHolonElementService().getHolonElements(holonObject);
+			int response=0;
+			for(int i=0;i< holonElementList.size();i++)
+			{
+				response=response+holonElementList.get(i).getCurrentCapacity();
+			}
+			getResponse().setContentType("text/html");
+			getResponse().getWriter().write(response+"*"+holonObjectId);
+		}
+		catch(Exception e)
+		{
+			log.debug("Error occurred in getting Holon Elements. Please check application Logs for more information.");
+			e.printStackTrace();
+		}
 	}
 }
